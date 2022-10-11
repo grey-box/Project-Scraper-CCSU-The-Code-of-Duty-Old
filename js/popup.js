f84989b0ac5fef68293a751ffeb6ae3fa65f3dfe
Jiaxin <13117340036@163.com>
2022-10-11 02:17:42 -0400

    changes based on merging (await images replacing)

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-// const context = require("node/ts4.8/util");
 let urlform = document.getElementById("url-form");
 let optform = document.getElementById("option-form");
 
@@ -32,130 +31,199 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   urlform.addEventListener("submit", (event) => {
     // Store url recieved from the form
     // let url = urlform.url.value;
+
     let html = null;
-    function download() {
+    let hostname = url.match(/(?<=(http|https):\/\/)(\S+?)(?=\/)/)[0];
+    let css = [];
+    let cssLinks = [];
+    let imgLinks = []; //length:20
+    let base64_array = []; //
+
+    // Function to download the html retrieved from jquery
+    function download(html) {
+      // Convert html into objectURL to use chrome.downloads
       let blob = new Blob([html], { type: "text/html" });
       objURL = URL.createObjectURL(blob);
 
       // Download the url object
+      chrome.downloads.download({
+        url: objURL,
+        saveAs: true,
+      });
+    }
 
-        chrome.downloads.download({
-          url: objURL,
-          saveAs: true,
-        });
-      }
+    // Asynchronous function to get data from a URL
+    let getData = async (url) => {
+      console.log("getData:", "Getting data from URL");
+      return $.get(url);
+    };
 
+    // Function to grab the <link> tags with the attribute = stylesheet from
+    // html and push them into cssLinks array
+    let getCSSLinks = (html) => {
+      PARSEDHTML = $.parseHTML(html);
+      linkElements = $(PARSEDHTML).filter("link[rel=stylesheet]");
+      linkElements.each(function () {
+        // Create a dummy element to transfer <link> tag href to an <a> tag
+        // so that JQuery can identify its protocol, hostname, and pathname etc.
+        let element = document.createElement("a");
+        $(element).attr("href", $(this).attr("href"));
 
-    // JQuery used to grab HTML from the url => response
-    $.get(url, function (response) {
-      // RegEx to grab the hostname of the URL
-      let hostname = url.match(/(?<=(http|https):\/\/)(\S+?)(?=\/)/)[0];
-      let css = [];
-      let cssLinks = [];
-
-      html = response;
-
-      //reg ex: get src and srcset
-      const reg1 = /(?<=img.*?src=)(".*?[jpg|png]")/gm
-      const reg2 = /(?<=img.*?srcset=)(".*?[jpg|png|JPG] 2x")/gm
-      //replace
-      const length_reg1 = html.match(reg1).length //20
-      const length_reg2 = html.match(reg2).length //20: including /static/images
-
-      //console.log(html.match(reg1))
-      //console.log(html.match(reg2))
-      let urls = [] //length:20
-      for(let i=0;i<length_reg1;i++){
-        urls.push("https:"+html.match(reg1)[i].substring(
-            html.match(reg1)[i].indexOf("\"") + 1,
-            html.match(reg1)[i].lastIndexOf("\"")))
-      }
-     // console.log(urls) //all images url with https:
-      //console.log(html.match(reg2));
-
-      //find a method to get base64 string
-      var base64_array = [] //18
-      for(let i = 0; i<urls.length-2;i++){
-        fetch(urls[i])
-            .then((res) => res.blob())
-            .then((blob) => {
-              // Read the Blob as DataURL using the FileReader API
-              const reader = new FileReader();
-              reader.onloadend = function () {
-                base64_array.push(reader.result)
-              };
-              reader.readAsDataURL(new Blob([blob], { type: blob.type }));
-            })
-      }
-
-      //wait for the images base64 to be saved to the array
-      setTimeout(()=>{
-        base64_array.forEach((each,index)=>{
-          console.log(index)
-          console.log(each)
-          html = html.replace(html.match(reg1)[0],"\""+each+"\"");
-          html = html.replace(html.match(reg2)[0],"\""+each+"\"");
-        })
-      }, 1000)
-
-      //wait for the html to replace the images
-      setTimeout(()=>{
-        // Using JQuery to make an array of link tags with rel = stylesheet
-        linkElements = $(html).filter("link[rel=stylesheet]");
-        linkElements.each(function () {
-          //console.log($(this).prop("outerHTML"));
-
-          // Create a dummy element to transfer <link> tag href to an <a> tag
-          // so that JQuery can identify its protocol, hostname, and pathname etc.
-          let element = document.createElement("a");
-          $(element).attr("href", $(this).attr("href"));
-
-          if (element.protocol === "chrome-extension:") {
-            element = element.toString().replace("chrome-extension:", "https:");
-          }
-          if (element.toString().search(chrome.runtime.id) >= 1) {
-            element = element.toString().replace(chrome.runtime.id, hostname);
-          }
-          cssLinks.push(element.toString());
-        });
-        // For every CSS address gets its CSS and append to HTML if any
-        if (cssLinks.length > 0) {
-          cssLinks.forEach(function (cssLink, i) {
-            // Clean up CSS link address
-            //cssLink = link.replace(/amp; *?/g, "");
-
-            $.get({
-              url: cssLink,
-              dataType: "text",
-              error: function () {
-                // If the link to retrieve CSS returns error then assign nothing
-                // to its index value in css array
-                css[i] = "";
-              },
-              success: function (cssText) {
-                // Create css element for <head> tag and push into array
-                cssElement = "<style>" + cssText + "</style> ";
-                css[i] = cssElement;
-
-                if (css.length == cssLinks.length) {
-                  linkElements.each(function (index) {
-                    //Replace previous <link> tag with new css <style> tag
-                    html = html.replace(
-                        /(\<link) ?(\S*) ?(\S*) ?(rel\="stylesheet") ?(\S*) ?(\S*)>/,
-                        css[index]
-                    );
-                  });
-
-                  download();
-                }
-              },
-            });
-          });
-        } else {
-          download();
+        if (element.protocol === "chrome-extension:") {
+          element = element.toString().replace("chrome-extension:", "https:");
         }
-      },5000) //wait a bit long, about 10-20 seconds
-    });
+        if (element.toString().search(chrome.runtime.id) >= 1) {
+          element = element.toString().replace(chrome.runtime.id, hostname);
+        }
+
+        console.log("getCSSLinks:", "Pushing");
+        cssLinks.push(element.toString());
+      });
+    };
+
+    // Asynchronous function to retrieve CSS from links in the cssLinks array
+    let getCSS = async (html) => {
+      for (let index = 0; index < cssLinks.length; index++) {
+        try {
+          // Waits for the function to fulfil promise then set data to cssText
+          let cssText = await getData(cssLinks[index]);
+
+          //console.log(cssText)
+
+          // Wrap data into <sytle> tags to append to html
+          cssElement = "<style>" + cssText + "</style> ";
+
+          console.log("getCSS (try):", "Assigning");
+          css[index] = cssElement;
+        } catch (err) {
+          console.log(err);
+          css[index] = "";
+        }
+      }
+    };
+
+    // Function to replace components within HTML with CSS retrieved
+    let replaceCSS = () => {
+      //console.log(css)
+      cssLinks.forEach((cssLink, index) => {
+        //Replace previous <link> tag with new css <style> tag
+        console.log("replaceCSS:", "Replacing");
+        html = html.replace(
+          /(\<link) ?(\S*) ?(\S*) ?(rel\="stylesheet") ?(\S*) ?(\S*)>/,
+          css[index]
+        );
+      });
+    };
+
+    let converToBase64 = async () => {
+      for (let i = 0; i < imgLinks.length-2; i++) { //18
+        let img = await fetch(imgLinks[i]);
+
+        console.log(img);
+
+        let test = await img.blob();
+
+        console.log(test);
+
+        const reader = new FileReader();
+        reader.onloadend = function () {
+          base64_array.push(reader.result);
+        };
+        reader.readAsDataURL(new Blob([test], { type: test.type }));
+      }
+    };
+
+    // Main Asynchronous function that initiates the scraping process
+    const scrape = async (url) => {
+      try {
+        // Wait for function to fulfill promise then set HTML data to
+        // variable
+        html = await getData(url);
+
+        console.log("parsing HTML");
+        testParse = $.parseHTML(html);
+        let testImageElements = $(testParse).find("img");
+
+        //console.log(testImageElements);
+
+        testImageElements.each(function () {
+          let src = $(this).attr("src");
+          let srcset = $(this).attr("srcset");
+
+          //console.log(src)
+
+          if (src.toString().search("https") == -1) {
+           // console.log(src.toString().search("https"));
+            //console.log(src);
+            src = "https:" + src;
+            //console.log(src);
+          }
+
+          imgLinks.push(src); //20
+        });
+
+        //reg ex: get src and srcset
+        const reg1 = /(?<=img.*?src=)(".*?[jpg|png]")/gm;
+        const reg2 = /(?<=img.*?srcset=)(".*?[jpg|png|JPG] 2x")/gm;
+        //replace
+        const length_reg1 = html.match(reg1).length; //20
+        const length_reg2 = html.match(reg2).length; //20: including /static/images
+
+        console.log(imgLinks);
+
+        await converToBase64();
+
+        console.log(base64_array); //total-2
+
+        const match1 = html.match(reg1)
+        const match2 = html.match(reg2)
+        console.log(match1)
+        console.log(match2)
+
+
+        //wait for replacing process
+        let replace = async (each,i)=>{
+          html = html.replace(match1[i], '"' + each + '"');
+          html = html.replace(match2[i], '"' + each + '"');
+        }
+
+        for(let k=0;k<base64_array.length;k++){
+            await replace(base64_array[k],k)
+        }
+
+        // base64_array.forEach((each, index) => { //18
+        //   console.log("For Each:", "Replacing image");
+        //   html = html.replace(html.match(reg1)[0], '"' + each + '"');
+        //   html = html.replace(html.match(reg2)[0], '"' + each + '"');
+        // });
+
+
+
+        // Initiate function to get CSS links
+        getCSSLinks(html);
+
+        // For every CSS link gets its CSS and append to HTML if any
+        if (cssLinks.length > 0) {
+          try {
+            await getCSS(html);
+
+            replaceCSS();
+
+            console.log("DOWNLOADING");
+            download(html);
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          download(html);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    scrape(url);
+
     event.preventDefault();
   });
 });
