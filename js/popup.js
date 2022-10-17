64a2d8954b45f3cba1629a0722cdeefb5dee7927
Jiaxin <13117340036@163.com>
2022-10-17 19:51:54 -0400

    save images to zip

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,7 @@
+
 let urlform = document.getElementById("url-form");
 let optform = document.getElementById("option-form");
+
 
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(
@@ -22,208 +24,272 @@ optform.aszip.addEventListener("change", (event) => {
   chrome.storage.local.set({ aszip: Boolean(event.target.checked) });
 });
 
-// Allows the extension to read the tabs that are open
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  // Retrieves the current tab URL
-  let url = tabs[0].url;
+//Execute after submitting form (clicking the save button)
 
-  // Execute after submitting form (clicking the save button)
-  urlform.addEventListener("submit", (event) => {
-    // Store url recieved from the form
-    // let url = urlform.url.value;
 
-    let html = null;
-    let hostname = url.match(/(?<=(http|https):\/\/)(\S+?)(?=\/)/)[0];
-    let css = [];
-    let cssLinks = [];
-    let imgLinks = []; //length:20
-    let base64_array = []; //
+urlform.addEventListener("submit", async (event) => {
+    let urls_file_content = []
+   // let allImagesList = [];
+    //read from input file
+    console.log('read file')
 
-    // Function to download the html retrieved from jquery
-    function download(html) {
-      // Convert html into objectURL to use chrome.downloads
-      let blob = new Blob([html], { type: "text/html" });
-      objURL = URL.createObjectURL(blob);
-
-      // Download the url object
-      chrome.downloads.download({
-        url: objURL,
-        saveAs: true,
-      });
+    function load(){
+        return new Promise(((resolve, reject) => {
+            let selected = document.getElementById("file").files[0];
+                    console.log(selected)
+                    let reader = new FileReader();
+                    reader.addEventListener("loadend", () => {
+                        urls_file_content = reader.result.split(/\r\n|\n/)
+                        resolve(urls_file_content)
+                        console.log("in promise")
+                        console.log(urls_file_content)
+                    });
+                    reader.readAsText(selected);
+        }))
     }
 
-    // Asynchronous function to get data from a URL
     let getData = async (url) => {
-      console.log("getData:", "Getting data from URL");
-      return $.get(url);
+        console.log("getData:", "Getting data from URL");
+        return $.get(url);
     };
 
-    // Function to grab the <link> tags with the attribute = stylesheet from
-    // html and push them into cssLinks array
-    let getCSSLinks = (html) => {
-      PARSEDHTML = $.parseHTML(html);
-      linkElements = $(PARSEDHTML).filter("link[rel=stylesheet]");
-      linkElements.each(function () {
-        // Create a dummy element to transfer <link> tag href to an <a> tag
-        // so that JQuery can identify its protocol, hostname, and pathname etc.
-        let element = document.createElement("a");
-        $(element).attr("href", $(this).attr("href"));
 
-        if (element.protocol === "chrome-extension:") {
-          element = element.toString().replace("chrome-extension:", "https:");
-        }
-        if (element.toString().search(chrome.runtime.id) >= 1) {
-          element = element.toString().replace(chrome.runtime.id, hostname);
-        }
+    // let getHtml = async (url) =>{
+    //     $.get(url,function (response){
+    //         try{
+    //             var find = '"//';
+    //             var re = new RegExp(find, 'g');
+    //             let html = response.replaceAll(re, "https://");
+    //             console.log()
+    //             return html
+    //         }catch (err){
+    //             console.log(err)
+    //             return response
+    //         }
+    //     })
+    // };
 
-        console.log("getCSSLinks:", "Pushing");
-        cssLinks.push(element.toString());
-      });
-    };
-
-    // Asynchronous function to retrieve CSS from links in the cssLinks array
-    let getCSS = async (html) => {
-      for (let index = 0; index < cssLinks.length; index++) {
+    const get_imgs = async (url) => {
+        let html;
         try {
-          // Waits for the function to fulfil promise then set data to cssText
-          let cssText = await getData(cssLinks[index]);
+            // Wait for function to fulfill promise then set HTML data to
+            // variable
+            let imgLinks = [];
+            html = await getData(url);
+            console.log("parsing HTML");
+            testParse = $.parseHTML(html);
+            let testImageElements = $(testParse).find("img");
+            // console.log(testImageElements);
+            testImageElements.each(function () {
+                let src = $(this).attr("src");
+                let srcset = $(this).attr("srcset");
+                //console.log(src)
+                if (src.toString().search("https") == -1) {
+                    // console.log(src.toString().search("https"));
+                    //console.log(src);
+                    src = "https:" + src;
+                    //console.log(src);
+                }
+                imgLinks.push(src);
+            });
+            return imgLinks;
+        } catch (e) {
+            console.error(e)
+        }}
 
-          //console.log(cssText)
-
-          // Wrap data into <sytle> tags to append to html
-          cssElement = "<style>" + cssText + "</style> ";
-
-          console.log("getCSS (try):", "Assigning");
-          css[index] = cssElement;
-        } catch (err) {
-          console.log(err);
-          css[index] = "";
-        }
-      }
-    };
-
-    // Function to replace components within HTML with CSS retrieved
-    let replaceCSS = () => {
-      //console.log(css)
-      cssLinks.forEach((cssLink, index) => {
-        //Replace previous <link> tag with new css <style> tag
-        console.log("replaceCSS:", "Replacing");
-        html = html.replace(
-          /(\<link) ?(\S*) ?(\S*) ?(rel\="stylesheet") ?(\S*) ?(\S*)>/,
-          css[index]
-        );
-      });
-    };
-
-    let converToBase64 = async () => {
-      for (let i = 0; i < imgLinks.length-2; i++) { //18
-        let img = await fetch(imgLinks[i]);
-
-        console.log(img);
-
-        let test = await img.blob();
-
-        console.log(test);
-
-        const reader = new FileReader();
-        reader.onloadend = function () {
-          base64_array.push(reader.result);
-        };
-        reader.readAsDataURL(new Blob([test], { type: test.type }));
-      }
-    };
-
-    // Main Asynchronous function that initiates the scraping process
-    const scrape = async (url) => {
-      try {
-        // Wait for function to fulfill promise then set HTML data to
-        // variable
-        html = await getData(url);
-
-        console.log("parsing HTML");
-        testParse = $.parseHTML(html);
-        let testImageElements = $(testParse).find("img");
-
-        //console.log(testImageElements);
-
-        testImageElements.each(function () {
-          let src = $(this).attr("src");
-          let srcset = $(this).attr("srcset");
-
-          //console.log(src)
-
-          if (src.toString().search("https") == -1) {
-           // console.log(src.toString().search("https"));
-            //console.log(src);
-            src = "https:" + src;
-            //console.log(src);
-          }
-
-          imgLinks.push(src); //20
+    function urlToPromise(url) {
+        return new Promise(function(resolve, reject) {
+            JSZipUtils.getBinaryContent(url, function (err, data) {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
         });
+    }
 
-        //reg ex: get src and srcset
-        const reg1 = /(?<=img.*?src=)(".*?[jpg|png]")/gm;
-        const reg2 = /(?<=img.*?srcset=)(".*?[jpg|png|JPG] 2x")/gm;
-        //replace
-        const length_reg1 = html.match(reg1).length; //20
-        const length_reg2 = html.match(reg2).length; //20: including /static/images
-
-        console.log(imgLinks);
-
-        await converToBase64();
-
-        console.log(base64_array); //total-2
-
-        const match1 = html.match(reg1)
-        const match2 = html.match(reg2)
-        console.log(match1)
-        console.log(match2)
-
-
-        //wait for replacing process
-        let replace = async (each,i)=>{
-          html = html.replace(match1[i], '"' + each + '"');
-          html = html.replace(match2[i], '"' + each + '"');
+    async function getContent(){
+        try{
+            var zip = new JSZip()
+            //get page urls from the txt
+            const res = await load()
+            for (const each of res) {
+                const index = res.indexOf(each);
+                let folder_name = each.substr(each.lastIndexOf('/') + 1)
+                console.log("folder_name:"+folder_name)
+                //create folder
+                zip.folder(folder_name)
+                const imgs = await get_imgs(each)
+                for(let i =0; i<imgs.length-2;i++){
+                    console.log(imgs[i])
+                    //create img file
+                    zip.file(folder_name+"/"+imgs[i].toString().substr(imgs[i].lastIndexOf('/') + 1), urlToPromise(imgs[i]), {binary:true});
+                }
+                //html (not modified)
+                zip.file(each.substr(each.lastIndexOf('/') + 1)+".html",getData(each))
+            }
+            zip.generateAsync({type:"blob"}).then((function (content){
+                console.log(content)
+                saveAs(content, "pages.zip");
+            }))
+        }catch (err){
+            console.error(err)
         }
+    }
 
-        for(let k=0;k<base64_array.length;k++){
-            await replace(base64_array[k],k)
-        }
+    getContent()
+// - Wikipedia_files
+/*    function loadPages(res){
+        let pagesContent = []
+        return new Promise(((resolve, reject) => {
+            if(res.length >0 ){
+                res.forEach((each)=>{
+                    fetch(each).then(function (data){
+                        pagesContent.push(data.text())
+                    })
+                })
+                resolve(pagesContent)
+            }
+            else{
+                reject("No urls")
+            }
+        }))
+    }*/
 
-        // base64_array.forEach((each, index) => { //18
-        //   console.log("For Each:", "Replacing image");
-        //   html = html.replace(html.match(reg1)[0], '"' + each + '"');
-        //   html = html.replace(html.match(reg2)[0], '"' + each + '"');
-        // });
-
-
-
-        // Initiate function to get CSS links
-        getCSSLinks(html);
-
-        // For every CSS link gets its CSS and append to HTML if any
-        if (cssLinks.length > 0) {
-          try {
-            await getCSS(html);
-
-            replaceCSS();
-
-            console.log("DOWNLOADING");
-            download(html);
-          } catch (err) {
-            console.log(err);
-          }
-        } else {
-          download(html);
-        }
-      } catch (err) {
-        console.log(err);
-      }
+   /* let getData = async (url) => {
+        console.log("getData:", "Getting data from URL");
+        return $.get(url);
     };
 
-    scrape(url);
+    async function writeIntoFile(pages) {
+        console.log(pages)
+        await fetch(pages)
 
-    event.preventDefault();
-  });
-});
+
+        /!*console.log("line 1")
+        var zip = new JSZip()
+        console.log("line 2")
+        return new Promise(((resolve, reject)=>{
+            console.log("line 3")
+            pages.forEach((each,index)=>{
+                console.log("line 4")
+                each.then(()=>{
+                    zip.file(index+".html",each)
+                }).catch((e)=>{
+                    console.error(e)
+                })
+            })
+            zip.generateAsync({type:"blob"}).then((function (content){
+                saveAs(content, "pages.zip");
+                resolve(content)
+            }))
+        }))*!/
+    }
+
+    async function getContent(){
+        try{
+            var zip = new JSZip()
+            const res = await load()
+            //console.log(res[0])
+            res.forEach((each, index)=>{
+                zip.file("file"+index+".html",getData(each))
+            })
+            zip.generateAsync({type:"blob"}).then((function (content){
+                console.log(content)
+                saveAs(content, "pages.zip");
+            }))
+
+            // console.log("get html")
+            // console.log(res)
+            // const pages = await loadPages(res)
+            // console.log("try to get pages")
+            // console.log(pages)
+            // const files = await writeIntoFile(pages)
+            // console.log("try to get zip")
+            // console.log(files)
+        }catch (err){
+            console.error(err)
+        }
+    }
+
+    getContent()*/
+    //
+    // load().then((message)=>{
+    //     console.log("after load")
+    //     console.log(message[0])
+    // })
+    //load file content
+ //   const loadFile = async () => {
+    //     try {
+    //         let selected = document.getElementById("file").files[0];
+    //         console.log(selected)
+    //         let reader = new FileReader();
+    //         reader.addEventListener("loadend", () => {
+    //             urls_file_content = reader.result.split(/\r\n|\n/)
+    //             console.log("before await")
+    //             console.log(urls_file_content)
+    //         });
+    //         reader.readAsText(selected);
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // }
+//get urls
+//     let getUrls = async ()=>{
+//         await loadFile()
+//         console.log("after await")
+//         console.log(urls_file_content.length)
+//         for(let i=0;i<urls_file_content.length-1;i++){
+//             let url_txt = await fetch(urls_file_content[i])
+//             console.log(url_txt)
+//         }
+//     }
+//
+//     await getUrls()
+})
+
+    // var reader = new FileReader();
+    // reader.onload = function(progressEvent){
+    //     urls_file_content = reader.result.split(/\r\n|\n/);
+    //     for(var line = 0; line < urls_file_content.length-1; line++){
+    //         console.log(urls_file_content[line]);
+    //     }
+    // };
+    // reader.readAsText(selected);
+
+
+
+    //downloading mutiple pages into a zip file
+    //read urls from a txt file
+// var fileContentArray = []
+//
+//     document.getElementById('file').onchange = function(){
+//         var file = this.files[0];
+//         var reader = new FileReader();
+//         reader.onload = function(progressEvent){
+//             var fileContentArray = this.result.split(/\r\n|\n/);
+//             for(var line = 0; line < fileContentArray.length-1; line++){
+//                 console.log(fileContentArray[line]);
+//             }
+//         };
+//         reader.readAsText(file);
+//     };
+
+
+
+
+
+    //search for each one
+
+
+    //save them into zip file
+
+
+
+
+   // event.preventDefault();
+//});
+
+
+
--- readme.md
+++ readme.md
@@ -1,31 +1,9 @@
-## Web Scraper
-### Images Grabbing
-
 ### Instructions
 
-#### version 1.0:
-using set time out: After clicking download button, wait for about 15 seconds to save the page
-##### outcomes: 
-some images are shown, some images are in the wrong locations 
+clcik upload
+Upload a file containing the pages urls you want to save
 
-#### version 2.0 (updated by 10/10/22):
-async await<br>  (based on merging branch)
-##### something new:
+For example:
+read_urls.txt
 
-```javascript
-const match1 = html.match(reg1) //save the match results as array
-const match2 = html.match(reg2)
-```
-
-```javascript
-let replace = async (each,i)=>{
-          html = html.replace(match1[i], '"' + each + '"');
-          html = html.replace(match2[i], '"' + each + '"');
-        }
-
-for(let k=0;k<base64_array.length;k++){
-    await replace(base64_array[k],k) //wait for the replacing process
-}
-```
-##### outcomes: 
-most of images are shown, and they are in the right locations. Some images are not shown, part of reason is that the base64 issue is too large so the request cannot be processed. Some imgaes are shown twice.
+save all the images to a folder for each page in zip file (in the same path as the file you uploaded)
