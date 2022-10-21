5ff0077cb87c2d1a3503b37c0f66d5baeb2bdb70
Jiaxin <13117340036@163.com>
2022-10-20 21:20:32 -0400

    zip images and html

--- js/popup.js
+++ js/popup.js
@@ -1,50 +1,50 @@
-
 let urlform = document.getElementById("url-form");
-let optform = document.getElementById("option-form");
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.local.get(
-    {
-      aszip: false,
-      corsbypass: false,
-    },
-    (data) => {
-      optform.corsbypass.checked = data.corsbypass;
-      optform.aszip.checked = data.aszip;
-    }
-  );
-});
-
-optform.corsbypass.addEventListener("change", (event) => {
-  chrome.storage.local.set({ corsbypass: Boolean(event.target.checked) });
-});
-
-optform.aszip.addEventListener("change", (event) => {
-  chrome.storage.local.set({ aszip: Boolean(event.target.checked) });
-});
+// let optform = document.getElementById("option-form");
+//
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//     chrome.storage.local.get(
+//         {
+//             aszip: false,
+//             corsbypass: false,
+//         },
+//         (data) => {
+//             optform.corsbypass.checked = data.corsbypass;
+//             optform.aszip.checked = data.aszip;
+//         }
+//     );
+// });
+//
+// optform.corsbypass.addEventListener("change", (event) => {
+//     chrome.storage.local.set({corsbypass: Boolean(event.target.checked)});
+// });
+//
+// optform.aszip.addEventListener("change", (event) => {
+//     chrome.storage.local.set({aszip: Boolean(event.target.checked)});
+// });
 
 //Execute after submitting form (clicking the save button)
 
+let saveBtn = document.querySelectorAll('.btn-secondary')
 
-urlform.addEventListener("submit", async (event) => {
+urlform.addEventListener("click", async (event) => {
     let urls_file_content = []
     let allImagesList = [];
     //read from input file
     console.log('read file')
 
-    function load(){
+    function load() {
         return new Promise(((resolve, reject) => {
             let selected = document.getElementById("file").files[0];
-                    console.log(selected)
-                    let reader = new FileReader();
-                    reader.addEventListener("loadend", () => {
-                        urls_file_content = reader.result.split(/\r\n|\n/)
-                        resolve(urls_file_content)
-                        console.log("in promise")
-                        console.log(urls_file_content)
-                    });
-                    reader.readAsText(selected);
+            console.log(selected)
+            let reader = new FileReader();
+            reader.addEventListener("loadend", () => {
+                urls_file_content = reader.result.split(/\r\n|\n/)
+                resolve(urls_file_content)
+                console.log("in promise")
+                console.log(urls_file_content)
+            });
+            reader.readAsText(selected);
         }))
     }
 
@@ -80,12 +80,13 @@ urlform.addEventListener("submit", async (event) => {
             return imgLinks;
         } catch (e) {
             console.error(e)
-        }}
+        }
+    }
 
     function urlToPromise(url) {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             JSZipUtils.getBinaryContent(url, function (err, data) {
-                if(err) {
+                if (err) {
                     reject(err);
                 } else {
                     resolve(data);
@@ -170,30 +171,6 @@ urlform.addEventListener("submit", async (event) => {
                     html = await getData(url);
                     console.log("await get data from url: before replacing")
                     console.log(html)
-
-                    // console.log("parsing HTML");
-                    // testParse = $.parseHTML(html);
-                    // let testImageElements = $(testParse).find("img");
-                    //
-                    // console.log(testImageElements);
-                    //
-                    // testImageElements.each(function () {
-                    //     let src = $(this).attr("src");
-                    //     let srcset = $(this).attr("srcset");
-                    //
-                    //     //console.log(src)
-                    //
-                    //     if (src.toString().search("https") == -1) {
-                    //         console.log(src.toString().search("https"));
-                    //         console.log(src);
-                    //         src = "https:" + src;
-                    //         console.log(src);
-                    //     }
-                    //
-                    //     imgLinks.push(src);
-                    // });
-
-                    //reg ex: get src and srcset
                     const reg1 = /(?<=img.*?src=)(".*?[jpg|png]")/gm;
                     const reg2 = /(?<=img.*?srcset=)(".*?[jpg|png|JPG] 2x")/gm;
                     const reg3 = /(?<=img.*?srcset)(=".*?[jpg|png|JPG] 2x")/gm
@@ -202,21 +179,28 @@ urlform.addEventListener("submit", async (event) => {
                     const length_reg2 = html.match(reg2).length; //20: including /static/images
                     const length_reg3 = html.match(reg3).length
 
-                    for(let j=0;j<length_reg3;j++){
-                        html = html.replace(html.match(reg3)[j],"")
+                    //remove srcset
+                    for (let j = 0; j < length_reg3 - 2; j++) {
+                        html = html.replace(html.match(reg3)[j], "")
                     }
 
-                    //console.log(imgLinks);
+                    for (let j = 0; j < html.match(reg3).length - 2; j++) {
+                        html = html.replace(html.match(reg3)[j], "")
+                    }
 
-                    //await converToBase64();
+                    for (let j = 0; j < html.match(reg3).length; j++) {
+                        html = html.replace(html.match(reg3)[0], "")
+                    }
 
-                    //console.log(base64_array);
 
+                    //replace src
                     for (let i = 0; i < imgUrlArray.length - 2; i++) {
-
-                        html = html.replace(html.match(reg1)[i], '"./' + folderName + "/"+imgUrlArray[i].toString().substr(imgUrlArray[i].lastIndexOf('/') + 1) + '"');
-                       //html = html.replace(html.match(reg2)[i], '"./' + folderName + "/"+imgUrlArray[i].toString().substr(imgUrlArray[i].lastIndexOf('/') + 1) + '"');
+                        html = html.replace(html.match(reg1)[i], '"./' + folderName + "/" + imgUrlArray[i].toString().substr(imgUrlArray[i].lastIndexOf('/') + 1) + '"');
+                        //html = html.replace(html.match(reg2)[i], '"./' + folderName + "/"+imgUrlArray[i].toString().substr(imgUrlArray[i].lastIndexOf('/') + 1) + '"');
                     }
+
+
+
                     // imgUrlArray.forEach((each, index) => {
                     //     console.log("For Each:", "Replacing image");
                     //     html = html.replace(html.match(reg1)[index], '"./' +folderName+ each.toString().substr(each.lastIndexOf('/') + 1) + '"');
@@ -252,127 +236,125 @@ urlform.addEventListener("submit", async (event) => {
             };
 
             await scrape(url);
-        }))}
+        }))
+    }
 
 
-    async function getContent(){
-        try{
+    async function getContent() {
+        try {
             var zip = new JSZip()
-            const res = await load() //get pages urls from txt file
-            //for each page
+            const res = await load()
+            let folder_name = "images_folder"
+            zip.folder(folder_name)
             for (const each of res) {
                 const index = res.indexOf(each);
-                let folder_name = each.substr(each.lastIndexOf('/') + 1)
-                console.log("folder_name:"+folder_name)
-                zip.folder(folder_name)
+                console.log("folder_name:" + folder_name)
                 const imgs = await get_imgs(each)
-                //for each image
-                for(let i =0; i<imgs.length-2;i++){
+                for (let i = 0; i < imgs.length - 2; i++) {
                     console.log(imgs[i])
                     let image_name = imgs[i].toString().substr(imgs[i].lastIndexOf('/') + 1)
-                    zip.file(folder_name+"/"+image_name, urlToPromise(imgs[i]), {binary:true});
+                    zip.file(folder_name + "/" + image_name, urlToPromise(imgs[i]), {binary: true});
                 }
-                
                 const html_new = await get_html(each, folder_name, imgs)
                 console.log(html_new)
-                //save all into a folder
-                //create a folder to save images
-                zip.file(each.substr(each.lastIndexOf('/') + 1)+".html",html_new)
+                zip.file(each.substr(each.lastIndexOf('/') + 1) + ".html", html_new)
             }
 
-            //save all
-            zip.generateAsync({type:"blob"}).then((function (content){
+            zip.generateAsync({type: "blob"}).then((function (content) {
                 console.log(content)
                 saveAs(content, "pages.zip");
             }))
-        }catch (err){
+        } catch (err) {
             console.error(err)
         }
     }
 
     getContent()
+    event.preventDefault();
+
+})
 // - Wikipedia_files
-/*    function loadPages(res){
-        let pagesContent = []
-        return new Promise(((resolve, reject) => {
-            if(res.length >0 ){
-                res.forEach((each)=>{
-                    fetch(each).then(function (data){
-                        pagesContent.push(data.text())
+    /*    function loadPages(res){
+            let pagesContent = []
+            return new Promise(((resolve, reject) => {
+                if(res.length >0 ){
+                    res.forEach((each)=>{
+                        fetch(each).then(function (data){
+                            pagesContent.push(data.text())
+                        })
                     })
-                })
-                resolve(pagesContent)
-            }
-            else{
-                reject("No urls")
-            }
-        }))
-    }*/
-
-   /* let getData = async (url) => {
-        console.log("getData:", "Getting data from URL");
-        return $.get(url);
-    };
-
-    async function writeIntoFile(pages) {
-        console.log(pages)
-        await fetch(pages)
-
-
-        /!*console.log("line 1")
-        var zip = new JSZip()
-        console.log("line 2")
-        return new Promise(((resolve, reject)=>{
-            console.log("line 3")
-            pages.forEach((each,index)=>{
-                console.log("line 4")
-                each.then(()=>{
-                    zip.file(index+".html",each)
-                }).catch((e)=>{
-                    console.error(e)
-                })
-            })
-            zip.generateAsync({type:"blob"}).then((function (content){
-                saveAs(content, "pages.zip");
-                resolve(content)
+                    resolve(pagesContent)
+                }
+                else{
+                    reject("No urls")
+                }
             }))
-        }))*!/
-    }
+        }*/
 
-    async function getContent(){
-        try{
-            var zip = new JSZip()
-            const res = await load()
-            //console.log(res[0])
-            res.forEach((each, index)=>{
-                zip.file("file"+index+".html",getData(each))
-            })
-            zip.generateAsync({type:"blob"}).then((function (content){
-                console.log(content)
-                saveAs(content, "pages.zip");
-            }))
+    /* let getData = async (url) => {
+         console.log("getData:", "Getting data from URL");
+         return $.get(url);
+     };
 
-            // console.log("get html")
-            // console.log(res)
-            // const pages = await loadPages(res)
-            // console.log("try to get pages")
-            // console.log(pages)
-            // const files = await writeIntoFile(pages)
-            // console.log("try to get zip")
-            // console.log(files)
-        }catch (err){
-            console.error(err)
-        }
-    }
+     async function writeIntoFile(pages) {
+         console.log(pages)
+         await fetch(pages)
 
-    getContent()*/
+
+         /!*console.log("line 1")
+         var zip = new JSZip()
+         console.log("line 2")
+         return new Promise(((resolve, reject)=>{
+             console.log("line 3")
+             pages.forEach((each,index)=>{
+                 console.log("line 4")
+                 each.then(()=>{
+                     zip.file(index+".html",each)
+                 }).catch((e)=>{
+                     console.error(e)
+                 })
+             })
+             zip.generateAsync({type:"blob"}).then((function (content){
+                 saveAs(content, "pages.zip");
+                 resolve(content)
+             }))
+         }))*!/
+     }
+
+     async function getContent(){
+         try{
+             var zip = new JSZip()
+             const res = await load()
+             //console.log(res[0])
+             res.forEach((each, index)=>{
+                 zip.file("file"+index+".html",getData(each))
+             })
+             zip.generateAsync({type:"blob"}).then((function (content){
+                 console.log(content)
+                 saveAs(content, "pages.zip");
+             }))
+
+             // console.log("get html")
+             // console.log(res)
+             // const pages = await loadPages(res)
+             // console.log("try to get pages")
+             // console.log(pages)
+             // const files = await writeIntoFile(pages)
+             // console.log("try to get zip")
+             // console.log(files)
+         }catch (err){
+             console.error(err)
+         }
+     }
+
+     getContent()*/
     //
     // load().then((message)=>{
     //     console.log("after load")
     //     console.log(message[0])
     // })
     //load file content
- //   const loadFile = async () => {
+    //   const loadFile = async () => {
     //     try {
     //         let selected = document.getElementById("file").files[0];
     //         console.log(selected)
@@ -399,21 +381,20 @@ urlform.addEventListener("submit", async (event) => {
 //     }
 //
 //     await getUrls()
-})
+//})
 
-    // var reader = new FileReader();
-    // reader.onload = function(progressEvent){
-    //     urls_file_content = reader.result.split(/\r\n|\n/);
-    //     for(var line = 0; line < urls_file_content.length-1; line++){
-    //         console.log(urls_file_content[line]);
-    //     }
-    // };
-    // reader.readAsText(selected);
+// var reader = new FileReader();
+// reader.onload = function(progressEvent){
+//     urls_file_content = reader.result.split(/\r\n|\n/);
+//     for(var line = 0; line < urls_file_content.length-1; line++){
+//         console.log(urls_file_content[line]);
+//     }
+// };
+// reader.readAsText(selected);
 
 
-
-    //downloading mutiple pages into a zip file
-    //read urls from a txt file
+//downloading mutiple pages into a zip file
+//read urls from a txt file
 // var fileContentArray = []
 //
 //     document.getElementById('file').onchange = function(){
@@ -429,18 +410,13 @@ urlform.addEventListener("submit", async (event) => {
 //     };
 
 
+//search for each one
 
 
-
-    //search for each one
-
-
-    //save them into zip file
+//save them into zip file
 
 
-
-
-   // event.preventDefault();
+// event.preventDefault();
 //});
 
 
